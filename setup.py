3750d41642d6e4fef26b9e763723bc2c50e7f63b
Weijie Wang <wang.weijie0110@gmail.com>
2025-05-17 14:20:18 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+APP = ['keytracker.py']  # your script filename
+DATA_FILES = []
+OPTIONS = {
+    'argv_emulation': True,
+    'includes': ['AppKit', 'Quartz', 'Foundation'],
+    'plist': {
+        'CFBundleName': 'Key Smasher',
+        'CFBundleDisplayName': 'Key Smasher',
+        'CFBundleIdentifier': 'com.yourname.keysmasher',
+        'CFBundleVersion': '0.1.0',
+        'LSUIElement': True,  # Prevents Dock icon
+    },
+}
+
+setup(
+    app=APP,
+    name='Key Smasher',
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
